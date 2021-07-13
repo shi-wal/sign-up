1df811b1c3ef73cbd78c50f74f0e6df692416650
Shivangi Agrawal <shivangiagrawal667@gmail.com>
2021-07-13 16:33:16 +0530

    Changed error show functionality

--- index.js
+++ index.js
@@ -11,19 +11,22 @@ function validateform() {
     var dotposition = email.lastIndexOf(".");
 
     if (firstname.length < 6 || firstname > 19) {
-        alert("First name must be greater than 5 letters and less than 20 letters.");
+        document.getElementById("fname_error").innerHTML="First name must be greater than 5 letters and less than 20 letters.";
         return false;
     }
+    document.getElementById("fname_error").innerHTML="";
 
     if (lastname.length < 6 || lastname > 19) {
-        alert("Last name must be greater than 5 letters and less than 20 letters.");
-        return false;
-    }    
-
-    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= email.length) {
-        alert("Please enter a valid e-mail address. For e.g.- example@mail.com");
+        document.getElementById("lname_error").innerHTML="Last name must be greater than 5 letters and less than 20 letters.";
         return false;
     }
+    document.getElementById("lname_error").innerHTML="";    
+
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= email.length) {
+        document.getElementById("email_error").innerHTML="Please enter a valid e-mail address. For e.g.- example@mail.com";
+        return false;
+    }
+    document.getElementById("email_error").innerHTML="";
         
     var myBirthday = new Date(dob);
     // set current day on 01:00:00 hours GMT+0100 (CET)
@@ -32,28 +35,36 @@ function validateform() {
     var myAge = ~~((Date.now(currentDate) - myBirthday) / (31557600000));
 
     if (myAge < 18) {
-        alert("DOB should be atleast 18 years back.");
-        return false;
-    }
-
+        document.getElementById("dob_error").innerHTML="DOB should be atleast 18 years back.";
+        
     if(age!=myAge){
-        alert("Your entered age is not getting matched with DOB.");
+        document.getElementById("age_error").innerHTML="Your entered age is not getting matched with DOB.";
         return false;
     }
+    }
+    document.getElementById("age_error").innerHTML="";
+    document.getElementById("dob_error").innerHTML="";
+    if(age!=myAge){
+        document.getElementById("age_error").innerHTML="Your entered age is not getting matched with DOB.";
+        return false;
+    }
+    document.getElementById("age_error").innerHTML="";
 
+    if (mobile.length != 10) {
+        document.getElementById("mobile_error").innerHTML+="Mobile no. should be of 10 digits. ";
+        return false;
+    }
+      document.getElementById("mobile_error").innerHTML="";
+
+    
     if (isNaN(mobile)){  
-        alert("Mobile no. should have only numeric digits.");
+        document.getElementById("mobile_error").innerHTML+="Enter numeric digits only. ";
         return false;  
       }
 
-    if (mobile.length != 10) {
-        alert("Mobile no. should be of 10 digits.");
-        return false;
-    }
-
 
     else {
-        window.location.href="index.html";
+        document.getElementById("mobile_error").innerHTML="";
         return true;
     }
 }
